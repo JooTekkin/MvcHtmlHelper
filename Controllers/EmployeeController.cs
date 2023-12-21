dd7f6160045cf3b4b8df29a1143d7576e0d2cd79
Yusuf Mustafa <yusufmustafa1@outlook.com>
2023-12-21 15:22:54 +0200

    Add Edit action, Edit view

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -3,7 +3,10 @@ using MvcPureHtml.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Validation;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -39,6 +42,41 @@ namespace MvcHtmlHelper.Controllers
             {
                 db.Employees.Add(employee);
                 db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Emp_Id)
+        {
+            var emp = db.Employees.FirstOrDefault(ww => ww.Emp_Id == Emp_Id);
+            var depts = db.Departments.ToList();
+            ViewBag.depts = new SelectList(depts, "Dept_Id", "Dept_Name");
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult edit( Employee employee)
+        {
+            ModelState.Remove("CPwd");
+            ModelState.Remove("Pwd");
+            if (ModelState.IsValid)
+            {
+                db.Employees.AddOrUpdate(employee);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch(DbEntityValidationException ex)
+                {
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.SaveChanges();
+                }
+                
                 return RedirectToAction("Index");
             }
             else
